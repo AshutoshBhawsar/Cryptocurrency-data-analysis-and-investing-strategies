a01eb9b3dcf4db963ff272d0d99968b5f8ef02e8
AshutoshBhawsar <ashutoshbhawsar98@gmail.com>
2022-12-02 17:27:06 -0500

    finalize phase 3 UI code

--- code/phase3.py
+++ code/phase3.py
@@ -1,3 +1,5 @@
+
+# Importing required libraries
 import streamlit as st
 from datetime import datetime
 
@@ -9,113 +11,116 @@ import numpy as np
 
 import joblib
 
-# from keras.models import load_model
-
 import pandas as pd
 
-# import matplotlib.pyplot as plt
 
-btc_main = pd.read_csv('../data/bitcoin_daily_data_cleaned.csv')#, index_col='Timestamp')
-
+# Setting Page title for web app
 st.set_page_config(page_title='Cryptocurrency Price Prediction App',
     layout='wide')
 
-st.title("Cryptocurrency Price Prediction")
+# Setting app heading as project name
+st.title("Cryptocurrency Data Analysis and Trading Strategy")
+
+# Reading cleaned data and printing a sample
+btc_main = pd.read_csv('../data/bitcoin_daily_data_cleaned.csv')#, index_col='Timestamp')
+btc_main.Timestamp = pd.to_datetime(btc_main.Timestamp).dt.date
 st.subheader("Dataset sample:")
 st.write(btc_main.tail())
 
 
-
-# st.subheader("1. LSTM")
-
+# Section 1 - Random Forest Classification for giving out trading calls
 st.subheader("1. Random Forest Classifier")
 # random_forest_classifier = joblib.load("../models/random_forest_classifier.joblib")
 random_forest_predictions = pd.read_csv('../data/random_forest_predictions.csv')
 
+# Getting input date for trade call
 st.markdown("**Please select date for getting trade call (Buy / Sell):**")
 input_date_rf = st.date_input('Input Date', value = datetime(2021,3,31).date())
 
-
+# Some error handling
 if input_date_rf is None:
     st.info('Please select date to predict')
 else:
     if input_date_rf < datetime(2019,11,14).date() or input_date_rf > datetime(2021,3,31).date():
         st.info("Date exceeds prediction limit")
     else:
+
+        # Getting predictions from model output
         random_forest_predictions.Timestamp = pd.to_datetime(random_forest_predictions.Timestamp).dt.date
         predicted_calls = random_forest_predictions[random_forest_predictions.Timestamp == input_date_rf]
 
 
-        # output
-        string_date = input_date_rf.strftime("%m/%d/%Y")
+        # output printing
+        string_date = input_date_rf.strftime("%Y/%m/%d")
         st.text(f"Trade signal for {string_date} is:")
         trade_call = predicted_calls.Pred.values[0]
         if trade_call == 1:
             st.info("BUY")
         else:
             st.info("SELL")
-        
-        # st.markdown(f"*Your portfolio of 1 BTC can have maximum equity of \${predicted_vals.yhat_upper.values[0].round(2)} and minimum equity of \${predicted_vals.yhat_lower.values[0].round(2)} on {string_date}*")
 
-
+# Printing out interactive graph of RF classifier output        
 st.line_chart(random_forest_predictions, x="Timestamp", y="Pred")
 
 
 
+##################################  SECTION BREAK  ##################################################
 
 
+
+# Section 2 - FaceBook Prophet model
 st.subheader("2. FB Prophet")
 
+# Retrieving model
 with open('../models/prophet_model.json', 'r') as fin:
     prophet_model = model_from_json(fin.read())
 
+# Pre loading the model output
 forecasting = pd.read_csv('../data/forecasting.csv')
-st.write(forecasting.tail())
+# st.write(forecasting)
 
+# Getting 3 inputs from user: investment date, liquidation date, and size of investment
 st.markdown("**Please select date for BTC prediction:**")
-# day = st.slider('Select day', 1, 30, 15, 1)
-# month = st.slider('Select month', 1, 12, 6, 1)
-# year = st.slider('Select year', 2012, 2021, 2020, 1)
-input_date = st.date_input('Date Input', value = datetime(2021,3,31).date())
+start_date = st.date_input('Investment date', value = datetime(2021,3,31).date())
+end_date = st.date_input('Liquidation date', value = datetime(2021,3,31).date())
+number_of_coins = st.number_input('Insert number of bitcoins to invest', min_value = 0.00, value = 0.00)
 
-
-if input_date is None:
-    st.info('Please select date to predict')
+# More Error Handling
+if number_of_coins == 0.00:
+    st.info("Please input all fields to proceed")
 else:
-    if input_date > datetime(2021,3,31).date():
+    if start_date > datetime(2021,3,31).date() or end_date > datetime(2021,3,31).date():
         st.info("Date exceeds prediction limit")
+    elif start_date > end_date:
+        st.info("Portfolio liquidation date cannot be before investment date, please try again")
     else:
+        # Retrieving portfolio value for investment start date
+        usd_price_start = btc_main[btc_main.Timestamp == start_date].Close.values[0]
+        # usd_price_end = btc_main[btc_main.Timestamp == end_date].Close.values[0]
+        portfolio_value = usd_price_start * number_of_coins
+        
+        # Printing out initial portfolio value
+        st.markdown(f"**Your initial portfolio value on {start_date} is \${portfolio_value}**")
+
+        # Getting model output for the given liquidation date
         forecasting.ds = pd.to_datetime(forecasting.ds).dt.date
-        # st.text(type(input_date))
-        # st.text(type(forecasting.ds.iloc[1]))
-        predicted_vals = forecasting[forecasting.ds == input_date]
-        # st.text(type(predicted_vals))
-        # st.text(pd.to_datetime(btc_main.Timestamp.iloc[1]).date())
+        predicted_vals = forecasting[forecasting.ds == end_date]
 
 
-        # output
-        string_date = input_date.strftime("%m/%d/%Y")
+        # Calculation and output for high, low, and predicted end portfolio value on liquidation
+        string_date = end_date.strftime("%Y/%m/%d")
         st.text("Predicted weighted price:")
-        st.info(predicted_vals.yhat.values[0])
+        st.info(predicted_vals.yhat.values[0] * number_of_coins)
         st.text("Predicted high value:")
-        st.info(predicted_vals.yhat_upper.values[0])
+        st.info(predicted_vals.yhat_upper.values[0] * number_of_coins)
         st.text("Predicted low value:")
-        st.info(predicted_vals.yhat_lower.values[0])
-        st.markdown(f"*Your portfolio of 1 BTC can have maximum equity of \${predicted_vals.yhat_upper.values[0].round(2)} and minimum equity of \${predicted_vals.yhat_lower.values[0].round(2)} on {string_date}*")
+        st.info(predicted_vals.yhat_lower.values[0] * number_of_coins)
 
+        # Output message from model to determine trading strategy
+        st.markdown(f"*Your portfolio of {number_of_coins} BTC can have maximum equity of \${predicted_vals.yhat_upper.values[0].round(2)} and minimum equity of \${predicted_vals.yhat_lower.values[0].round(2)} on {string_date}*")
 
+        # Printing interactive output graph of prophet model for better insights on time series
+        fig2 = plot_plotly(prophet_model, forecasting)
+        st.plotly_chart(fig2)
 
-fig2 = plot_plotly(prophet_model, forecasting)
-st.plotly_chart(fig2)
-
-# plt.title('Price Forecasting for Bitcoin')
-# plt.ylabel('Price (USD)')
-# plt.xlabel('Date')
-
-
-
-
-
-
-
-
+#######################################  END  #######################################################
